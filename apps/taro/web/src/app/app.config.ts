105e86aabb3982f5e96083031558b372a66c905f
Aleksandr Serenko <fafnur@yandex.ru>
2024-11-29 14:51:47 +0700

    [workspace] Config workspace

--- apps/taro/web/src/app/app.config.ts
+++ apps/taro/web/src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import type { ApplicationConfig } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideClientHydration(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-  ],
+  providers: [provideClientHydration(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
 };
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 const nx = require('@nx/eslint-plugin');
+const eslintPluginSimpleImportSort = require('eslint-plugin-simple-import-sort');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintConfigPrettier = require('eslint-config-prettier');
 
 module.exports = [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/public'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -28,6 +31,99 @@ module.exports = [
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
-    rules: {},
+    plugins: {
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      import: eslintPluginImport,
+    },
+    rules: {
+      complexity: 'error',
+      'no-new-wrappers': 'error',
+      'no-throw-literal': 'error',
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'enumMember',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'property',
+          format: null,
+          filter: {
+            regex: '^(host)$',
+            match: false,
+          },
+        },
+      ],
+      'sort-imports': 'off',
+      'import/no-unresolved': 'off',
+      'import/named': 'off',
+      'import/first': 'off',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^\\u0000'], ['^@?(?!taro)\\w'], ['^@taro?\\w'], ['^\\w'], ['^[^.]'], ['^\\.']],
+        },
+      ],
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
   },
+  {
+    files: ['**/*.stories.ts'],
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'enumMember',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'property',
+          format: null,
+          filter: {
+            regex: '^(host)$',
+            match: false,
+          },
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
 ];
