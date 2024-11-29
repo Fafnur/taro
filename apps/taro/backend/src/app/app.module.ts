86285604b52e936a07a3571b16c7cdeef5b18641
Aleksandr Serenko <fafnur@yandex.ru>
2024-11-29 15:21:12 +0700

    [backend] Adding backend

---
+++ apps/taro/backend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'node:path';
+
+import { AppController } from './app.controller';
+import { configurationFactory, typeOrmFactory } from './config/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: resolve('apps/taro/backend/.env'),
+      isGlobal: true,
+      load: [configurationFactory],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmFactory,
+    }),
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
