b6bbfa4631db44577cce400eebf8c2099e516069
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-17 22:25:30 +0700

    [ui][icons] Adding brand icon

---
+++ libs/ui/icons/src/lib/icon-brand/icon-brand.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'taro-icon-brand',
+  templateUrl: './icon-brand.component.html',
+  styleUrl: './icon-brand.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconBrandComponent {}
--- libs/web/home/page/src/lib/home-page.component.ts
+++ libs/web/home/page/src/lib/home-page.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { PATHS } from '@taro/core';
+import { IconBrandComponent } from '@taro/ui/icons';
 
 @Component({
   selector: 'taro-home-page',
-  imports: [RouterLink],
+  imports: [RouterLink, IconBrandComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
