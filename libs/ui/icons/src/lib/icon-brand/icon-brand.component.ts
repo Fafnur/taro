d555fcce0892e138708a394e358c8cb815f1f1d8
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-18 00:04:45 +0700

    [ui][home] Change home

--- libs/ui/icons/src/lib/icon-brand/icon-brand.component.ts
+++ libs/ui/icons/src/lib/icon-brand/icon-brand.component.ts
@@ -5,5 +5,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './icon-brand.component.html',
   styleUrl: './icon-brand.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'taro-icon-brand',
+  },
 })
 export class IconBrandComponent {}
